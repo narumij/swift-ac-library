79fd3ffe150af8e3e060c968356cd35936d3e65f
narumij <narumij@gmail.com>
2023-11-07 20:45:17 +0900

    range operator

--- Sources/atcoder/util/CPPClosedRange.swift
+++ Sources/atcoder/util/CPPClosedRange.swift
@@ -1,89 +1,46 @@
 import Foundation
 
-/// C++のループの挙動を、Swiftで楽に再現するためのもの。
-///
-/// 例えば、C++で以下の場合を、
-/// ```
-/// int r = 0;
-/// for (int n = 1; n <= r; n++) { }
-/// ```
-/// Swiftで
-/// ```
-/// var r = 0
-/// for n in 1...r { }
-/// ```
-/// とした場合、fatalErrorとなる。
-/// これを、
-/// ```
-/// var r = 0
-/// for n in 1...max(1,r) { }
-/// ```
-/// とした場合、C++は0回実行だが、Swiftでは1回実行してしまう。
-///
-/// この差異を吸収するために、演算子として以下を追加し、同じ挙動となるようにした。
-/// ```
-/// var r = 0
-/// for n in 1..<=r { }
-/// ```
-///
-/// 以下も追加してある。
-/// ```
-/// var r = 0
-/// for n in r..>=1 { }
-/// ```
-struct CPPClosedRange<Bound> where Bound: Comparable & Numeric {
-    let start: Bound
-    let end: Bound
-}
-
-extension CPPClosedRange: Sequence {
-    typealias Element = Bound
-    func makeIterator() -> Iterator { Iterator(start: start, end: end) }
-    struct Iterator: IteratorProtocol {
-        init(start current: Bound, end: Bound) {
-            self.end = end; self.current = current
-        }
-        private let end: Bound
-        private var current: Bound
-        mutating func next() -> Bound? {
-            guard current <= end else { return nil }
-            defer { current += 1 }
-            return current
-        }
-    }
-}
+// C++のループの挙動を、Swiftで楽に再現するためのもの。
+//
+// 例えば、C++で以下の場合を、
+// ```
+// int r = 0;
+// for (int n = 1; n <= r; n++) { }
+// ```
+// Swiftで
+// ```
+// var r = 0
+// for n in 1...r { }
+// ```
+// とした場合、fatalErrorとなる。
+// これを、
+// ```
+// var r = 0
+// for n in 1...max(1,r) { }
+// ```
+// とした場合、C++は0回実行だが、Swiftでは1回実行してしまう。
+//
+// この差異を吸収するために、演算子として以下を追加し、同じ挙動となるようにした。
+// ```
+// var r = 0
+// for n in 1..<=r { }
+// ```
+//
+// 以下も追加してある。
+// ```
+// var r = 0
+// for n in r..>=1 { }
+// ```
 
 infix operator ..<=: RangeFormationPrecedence
 
-func ..<= <Bound: Comparable>(lhs: Bound, rhs: Bound) -> CPPClosedRange<Bound> {
-    CPPClosedRange(start: lhs, end: rhs)
-}
-
-struct CPPClosedRangeReversed<Bound> where Bound: Comparable & Numeric {
-    let start: Bound
-    let end: Bound
-}
-
-extension CPPClosedRangeReversed: Sequence {
-    typealias Element = Bound
-    func makeIterator() -> Iterator { Iterator(start: start, end: end) }
-    struct Iterator: IteratorProtocol {
-        init(start current: Bound, end: Bound) {
-            self.end = end; self.current = current
-        }
-        private let end: Bound
-        private var current: Bound
-        mutating func next() -> Bound? {
-            guard current >= end else { return nil }
-            defer { current -= 1 }
-            return current
-        }
-    }
+func ..<= <Bound: Comparable>(lhs: Bound, rhs: Bound) -> StrideThrough<Bound> {
+    stride(from: lhs, through: rhs, by: 1)
 }
 
 infix operator ..>=: RangeFormationPrecedence
 
-func ..>= <Bound: Comparable>(lhs: Bound, rhs: Bound) -> CPPClosedRangeReversed<Bound> {
-    CPPClosedRangeReversed(start: lhs, end: rhs)
+func ..>= <Bound: Comparable>(lhs: Bound, rhs: Bound) -> StrideThrough<Bound> {
+    stride(from: lhs, through: rhs, by: -1)
 }
 
