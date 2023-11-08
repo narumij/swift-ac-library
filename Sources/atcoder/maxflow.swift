ff9d491c6d114db58b5b17447af8ee81f8949cf6
narumij <narumij@gmail.com>
2023-11-08 23:30:50 +0900

    memo

--- Sources/atcoder/maxflow.swift
+++ Sources/atcoder/maxflow.swift
@@ -1,6 +1,9 @@
 import Foundation
 import Collections
 
+// AC - https://atcoder.jp/contests/abc318/submissions/47384972
+
+// https://github.com/atcoder/ac-library/blob/master/atcoder/maxflow.hpp
 struct mf_graph<Cap: FixedWidthInteger> {
 //  public:
     init() { _n = 0; g = [] }
