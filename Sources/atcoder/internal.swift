e055be2b12757be7e80fcaa3cad29e23827c7e9f
narumij <narumij@gmail.com>
2023-11-11 17:18:30 +0900

    bit

--- Sources/atcoder/internal.swift
+++ Sources/atcoder/internal.swift
@@ -2,6 +2,41 @@ import Foundation
 
 enum `internal` { }
 
+extension `internal` {
+    static func bit_ceil(_ n: CUnsignedInt) -> CUnsignedInt {
+        var x: CUnsignedInt = 1;
+        while (x < n) { x *= 2; }
+        return x;
+    }
+
+    static func bit_ceil(_ n: Int) -> Int {
+        Int(bit_ceil(CUnsignedInt(n)))
+    }
+
+    static func countr_zero(_ x: CUnsignedInt) -> CInt {
+        if x == 0 { return 32 }
+        var n: CUnsignedInt = 1
+        var x = x
+        if x & 0x0000FFFF == 0 { n = n + 16; x = x >> 16 }
+        if x & 0x000000FF == 0 { n = n + 8;  x = x >> 8  }
+        if x & 0x0000000F == 0 { n = n + 4;  x = x >> 4  }
+        if x & 0x00000003 == 0 { n = n + 2;  x = x >> 2  }
+        return CInt(n - x & 1)
+    }
+
+    static func countr_zero(_ x: UInt) -> Int {
+        if x == 0 { return 64 }
+        var n: UInt = 1
+        var x = x
+        if x & 0xFFFFFFFF == 0 { n = n + 32; x = x >> 32 }
+        if x & 0x0000FFFF == 0 { n = n + 16; x = x >> 16 }
+        if x & 0x000000FF == 0 { n = n + 8;  x = x >> 8  }
+        if x & 0x0000000F == 0 { n = n + 4;  x = x >> 4  }
+        if x & 0x00000003 == 0 { n = n + 2;  x = x >> 2  }
+        return Int(n - x & 1)
+    }
+}
+
 extension segtree {
     enum `internal` { }
 }
