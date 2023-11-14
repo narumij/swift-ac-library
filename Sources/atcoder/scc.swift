7456358c505d315af9cfe4e86e345cad14270ff2
narumij <narumij@gmail.com>
2023-11-14 22:15:14 +0900

    read

--- Sources/atcoder/scc.swift
+++ Sources/atcoder/scc.swift
@@ -1,11 +1,11 @@
 import Foundation
 
-struct scc_graph {
+public struct scc_graph {
 //  public:
-    init() { `internal` = _internal.scc_graph.init(0) }
-    init<Index: FixedWidthInteger>(_ n: Index) { `internal` = _internal.scc_graph.init(Int(n)) }
+    public init() { `internal` = _internal.scc_graph.init(0) }
+    public init<Index: FixedWidthInteger>(_ n: Index) { `internal` = _internal.scc_graph.init(Int(n)) }
 
-    mutating func add_edge<Index: FixedWidthInteger>(_ from: Index,_ to: Index) {
+    public mutating func add_edge<Index: FixedWidthInteger>(_ from: Index,_ to: Index) {
         let from = Int(from)
         let to = Int(to)
         let n = `internal`.num_vertices();
@@ -14,7 +14,7 @@ struct scc_graph {
         `internal`.add_edge(from, to);
     }
 
-    func scc() -> [[Int]] { return `internal`.scc(); }
+    public func scc() -> [[Int]] { return `internal`.scc(); }
 
 //  private:
     var `internal`: _internal.scc_graph;
