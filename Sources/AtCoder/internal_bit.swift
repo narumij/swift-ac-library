1af82575148afa073b5c705d182c8442e556ad3b
narumij <narumij@gmail.com>
2024-01-30 07:05:03 +0900

    bit

--- Sources/AtCoder/internal_bit.swift
+++ Sources/AtCoder/internal_bit.swift
@@ -3,10 +3,10 @@ import Foundation
 extension _Internal {
     
     @usableFromInline
-    static func bit_ceil<SINT: FixedWidthInteger>(_ n: UInt32) -> SINT {
+    static func bit_ceil<INT: FixedWidthInteger>(_ n: UInt32) -> INT {
         var x: CUnsignedInt = 1
         while (x < n) { x *= 2 }
-        return SINT(x)
+        return INT(x)
     }
     
     @usableFromInline
@@ -17,15 +17,15 @@ extension _Internal {
     }
 
     @usableFromInline
-    static func countr_zero<SINT: SignedInteger>(_ x: UInt32) -> SINT {
+    static func countr_zero<INT: FixedWidthInteger>(_ x: UInt32) -> INT {
         if x == 0 { return 32 }
         var n: UInt32 = 1
         var x = x
         if x & 0x0000FFFF == 0 { n = n + 16; x = x >> 16 }
-        if x & 0x000000FF == 0 { n = n + 8;  x = x >> 8  }
-        if x & 0x0000000F == 0 { n = n + 4;  x = x >> 4  }
-        if x & 0x00000003 == 0 { n = n + 2;  x = x >> 2  }
-        return SINT(n - x & 1)
+        if x & 0x000000FF == 0 { n = n +  8; x = x >>  8 }
+        if x & 0x0000000F == 0 { n = n +  4; x = x >>  4 }
+        if x & 0x00000003 == 0 { n = n +  2; x = x >>  2 }
+        return INT(n - x & 1)
     }
     
     @usableFromInline
@@ -35,9 +35,9 @@ extension _Internal {
         var x = x
         if x & 0xFFFFFFFF == 0 { n = n + 32; x = x >> 32 }
         if x & 0x0000FFFF == 0 { n = n + 16; x = x >> 16 }
-        if x & 0x000000FF == 0 { n = n + 8; x = x >> 8 }
-        if x & 0x0000000F == 0 { n = n + 4; x = x >> 4 }
-        if x & 0x00000003 == 0 { n = n + 2; x = x >> 2 }
+        if x & 0x000000FF == 0 { n = n +  8; x = x >>  8 }
+        if x & 0x0000000F == 0 { n = n +  4; x = x >>  4 }
+        if x & 0x00000003 == 0 { n = n +  2; x = x >>  2 }
         return Int(n - x & 1)
     }
 }
