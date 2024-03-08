82cdf9ae92601f109e66e7a5675e01e33abde2d4
narumij <narumij@gmail.com>
2024-03-09 01:05:02 +0900

    bit

--- Sources/AtCoder/internal_bit.swift
+++ Sources/AtCoder/internal_bit.swift
@@ -5,24 +5,12 @@ extension _Internal {
     /// @return same with std::bit::bit_ceil
     @usableFromInline
     static func bit_ceil<INT: FixedWidthInteger>(_ n: UInt32) -> INT {
-#if false
-        var x: CUnsignedInt = 1
-        while x < n { x *= 2 }
-        return INT(x)
-#else
         INT(n <= 1 ? 1 : 1 << (UInt32.bitWidth - (n - 1).leadingZeroBitCount))
-#endif
     }
     
     @usableFromInline
     static func bit_ceil(_ n: UInt64) -> Int {
-#if false
-        var x = 1
-        while x < n { x *= 2 }
-        return x
-#else
         n <= 1 ? 1 : 1 << (UInt64.bitWidth - (n - 1).leadingZeroBitCount)
-#endif
     }
 
     /// @param n `1 <= n`
@@ -32,18 +20,11 @@ extension _Internal {
         INT(x.trailingZeroBitCount)
     }
     
-#if false
-    static var memoize_countr_zero: Memoized<UInt32,UInt32> = .init(source: countr_zero)
     /// @param n `1 <= n`
     /// @return same with std::bit::countr_zero
     static func countr_zero_constexpr<INT: FixedWidthInteger>(_ x: UInt32) -> INT {
-        INT(memoize_countr_zero.get(x))
-    }
-#else
-    static func countr_zero_constexpr<INT: FixedWidthInteger>(_ x: UInt32) -> INT {
         countr_zero(x)
     }
-#endif
 
     @usableFromInline
     static func countr_zero(_ x: UInt64) -> Int {
