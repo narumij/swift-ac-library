53f2cb7db5b96355e6b407b747de9ed38f2b6a6f
narumij <narumij@gmail.com>
2024-03-09 01:08:52 +0900

    bit

--- Sources/AtCoder/internal_bit.swift
+++ Sources/AtCoder/internal_bit.swift
@@ -3,30 +3,31 @@ import Foundation
 extension _Internal {
     
     /// @return same with std::bit::bit_ceil
-    @usableFromInline
+    @inlinable
     static func bit_ceil<INT: FixedWidthInteger>(_ n: UInt32) -> INT {
         INT(n <= 1 ? 1 : 1 << (UInt32.bitWidth - (n - 1).leadingZeroBitCount))
     }
     
-    @usableFromInline
+    @inlinable
     static func bit_ceil(_ n: UInt64) -> Int {
         n <= 1 ? 1 : 1 << (UInt64.bitWidth - (n - 1).leadingZeroBitCount)
     }
 
     /// @param n `1 <= n`
     /// @return same with std::bit::countr_zero
-    @usableFromInline
+    @inlinable
     static func countr_zero<INT: FixedWidthInteger>(_ x: UInt32) -> INT {
         INT(x.trailingZeroBitCount)
     }
     
     /// @param n `1 <= n`
     /// @return same with std::bit::countr_zero
+    @inlinable
     static func countr_zero_constexpr<INT: FixedWidthInteger>(_ x: UInt32) -> INT {
         countr_zero(x)
     }
 
-    @usableFromInline
+    @inlinable
     static func countr_zero(_ x: UInt64) -> Int {
         x.trailingZeroBitCount
     }
