9b8019417089174b4c8b44b39f003ba1b93d237f
narumij <narumij@gmail.com>
2024-11-09 18:07:51 +0900

    fix ci

--- Sources/AtCoder/internal_math.swift
+++ Sources/AtCoder/internal_math.swift
@@ -55,12 +55,16 @@ public struct barrett {
     // ((ab * im) >> 64) == c or c + 1
     var z = UInt(a)
     z &*= UInt(b)
+#if false
     let x: UInt
     if #available(macOS 15.0, *) {
       x = UInt((UInt128(z) * UInt128(im)) >> 64)
     } else {
       x = z.multipliedFullWidth(by: im).high
     }
+#else
+    let x: UInt = z.multipliedFullWidth(by: im).high
+#endif
     let y = x &* m
     return Unsigned(z &- y &+ (z < y ? m : 0))
   }
