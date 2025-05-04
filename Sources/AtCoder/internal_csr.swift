9af8b06b3f63301d8f34adc5bed3a63de567eedf
narumij <narumij@gmail.com>
2025-05-04 12:18:48 +0900

    scc

--- Sources/AtCoder/internal_csr.swift
+++ Sources/AtCoder/internal_csr.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+#if true
 extension _Internal {
 
   public struct csr<E> {
@@ -14,14 +15,14 @@ extension _Internal {
     @inline(__always)
     init(_ n: Int, _ edges: [(first: Int, second: E)]) {
       var start = [Int](repeating: 0, count: n + 1)
+      for e in edges {
+        start[e.first + 1] += 1
+      }
+      for i in stride(from: 1, through: n, by: 1) {
+        start[i] += start[i - 1]
+      }
+      var counter = start
       let elist = [E](unsafeUninitializedCapacity: edges.count) { elist, elistCount in
-        for e in edges {
-          start[e.first + 1] += 1
-        }
-        for i in stride(from: 1, through: n, by: 1) {
-          start[i] += start[i - 1]
-        }
-        var counter = start
         for e in edges {
           elist[counter[e.first]] = e.second
           counter[e.first] += 1
@@ -33,3 +34,89 @@ extension _Internal {
     }
   }
 }
+#else
+extension _Internal {
+
+  public struct csr<E> {
+    
+    @usableFromInline
+    let _start: Buffer<Int>
+    @usableFromInline
+    let _elist: Buffer<E>
+
+    @usableFromInline
+    var start: UnsafeMutablePointer<Int> { _start.pointer }
+
+    @usableFromInline
+    var elist: UnsafeMutablePointer<E> { _elist.pointer }
+
+    @inlinable
+    @inline(__always)
+    init(_ n: Int, _ edges: [(first: Int, second: E)]) {
+      _start = Buffer<Int>.create(withCapacity: n + 1)
+      _elist = Buffer<E>.create(withCapacity: edges.count)
+      start.initialize(repeating: 0, count: n)
+      for e in edges {
+        start[e.first + 1] += 1
+      }
+      for i in stride(from: 1, through: n, by: 1) {
+        start[i] += start[i - 1]
+      }
+      withUnsafeTemporaryAllocation(of: Int.self, capacity: n + 1) { counter in
+        counter.baseAddress?.initialize(from: start, count: n + 1)
+        for e in edges {
+          elist[counter[e.first]] = e.second
+          counter[e.first] += 1
+        }
+      }
+    }
+  }
+}
+
+extension _Internal.csr {
+  
+  @usableFromInline
+  struct Header {
+    @inlinable
+    @inline(__always)
+    internal init(capacity: Int) {
+      self.capacity = capacity
+    }
+    @usableFromInline var capacity: Int
+  }
+
+  @usableFromInline
+  class Buffer<Element>: ManagedBuffer<Header, Element> {
+    
+    @inlinable
+    deinit {
+      self.withUnsafeMutablePointers { header, elements in
+        elements.deinitialize(count: header.pointee.capacity)
+        header.deinitialize(count: 1)
+      }
+    }
+    @inlinable
+    @inline(__always)
+    var pointer: UnsafeMutablePointer<Element> {
+      withUnsafeMutablePointerToElements({ $0 })
+    }
+    
+    @inlinable
+    public subscript(index: Int) -> Element {
+      get { pointer[index] }
+      _modify { yield &pointer[index] }
+    }
+    
+    @inlinable
+    @inline(__always)
+    internal static func create(
+      withCapacity capacity: Int
+    ) -> Buffer<Element> {
+      let storage = Buffer<Element>.create(minimumCapacity: capacity) { _ in
+        Header(capacity: capacity)
+      }
+      return unsafeDowncast(storage, to: Buffer<Element>.self)
+    }
+  }
+}
+#endif
--- Sources/AtCoder/scc.swift
+++ Sources/AtCoder/scc.swift
@@ -2,25 +2,25 @@ import Foundation
 
 public struct SCCGraph {
 
-  @inlinable
+  @inlinable @inline(__always)
   public init() { _internal = .init(0) }
 
-  @inlinable
+  @inlinable @inline(__always)
   public init(_ n: Int) { _internal = .init(n) }
 
-  @inlinable
+  @inlinable @inline(__always)
   public mutating func add_edge(_ from: Int, _ to: Int) {
+#if DEBUG
     let n = _internal.num_vertices()
     assert(0 <= from && from < n)
     assert(0 <= to && to < n)
+#endif
     _internal.add_edge(from, to)
   }
 
-  @inlinable
+  @inlinable @inline(__always)
   public func scc() -> [[Int]] { return _internal.scc() }
 
-  // CIntをしばらくつかっていたが、キャストのオーバーヘッドを懸念して、Intに切り替えている(2025/1/2)
-  // TODO: 十分にテストできたのち、scc_graphの型変数を取り除くリファクタリングをすること
   @usableFromInline
-  var _internal: _Internal.scc_graph<Int>
+  var _internal: _Internal.scc_graph
 }
