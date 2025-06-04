bf3379a365a67a265bb868139e586d97dd71fa69
narumij <narumij@gmail.com>
2025-06-05 02:46:58 +0900

    csr

--- Sources/AtCoder/internal_csr.swift
+++ Sources/AtCoder/internal_csr.swift
@@ -1,6 +1,5 @@
 import Foundation
 
-#if true
 extension _Internal {
 
   @frozen
@@ -13,117 +12,37 @@ extension _Internal {
     var elist: [E]
 
     @inlinable
-    @inline(__always)
+    @inline(never)
     init(_ n: Int, _ edges: [(first: Int, second: E)]) {
-      var start = [Int](repeating: 0, count: n + 1)
-      for e in edges {
-        start[e.first + 1] += 1
-      }
-      for i in stride(from: 1, through: n, by: 1) {
-        start[i] += start[i - 1]
-      }
-      let elist = [E](unsafeUninitializedCapacity: edges.count) { elist, elistCount in
-        var counter = start
-        for e in edges {
-          elist[counter[e.first]] = e.second
-          counter[e.first] += 1
+      
+      (start, elist) = edges.withUnsafeBufferPointer { edges in
+
+        let start = [Int](unsafeUninitializedCapacity: n + 1) { start, startCount in
+          start.baseAddress?.initialize(repeating: 0, count: n + 1)
+          for e in edges {
+            start[e.first + 1] += 1
+          }
+          for i in stride(from: 1, through: n, by: 1) {
+            start[i] += start[i - 1]
+          }
+          startCount = n + 1
         }
-        elistCount = edges.count
-      }
-      self.start = start
-      self.elist = elist
-    }
-  }
-}
-#else
-extension _Internal {
 
-  @frozen
-  public struct csr<E> {
-    
-    @usableFromInline
-    let _start: Buffer<Int>
-    @usableFromInline
-    let _elist: Buffer<E>
-
-    @usableFromInline
-    var start: UnsafeMutablePointer<Int> { _start.pointer }
-
-    @usableFromInline
-    var elist: UnsafeMutablePointer<E> { _elist.pointer }
-
-    @inlinable
-    @inline(__always)
-    init(_ n: Int, _ edges: [(first: Int, second: E)]) {
-      _start = Buffer<Int>.create(withCapacity: n + 1)
-      _elist = Buffer<E>.create(withCapacity: edges.count)
-      start.initialize(repeating: 0, count: n)
-      for e in edges {
-        start[e.first + 1] += 1
-      }
-      for i in stride(from: 1, through: n, by: 1) {
-        start[i] += start[i - 1]
-      }
-      withUnsafeTemporaryAllocation(of: Int.self, capacity: n + 1) { counter in
-        counter.baseAddress?.initialize(from: start, count: n + 1)
-        for e in edges {
-          elist[counter[e.first]] = e.second
-          counter[e.first] += 1
+        let elist = [E](unsafeUninitializedCapacity: edges.count) { elist, elistCount in
+          withUnsafeTemporaryAllocation(of: Int.self, capacity: n + 1) { counter in
+            start.withUnsafeBufferPointer { start in
+              counter.baseAddress?.initialize(from: start.baseAddress!, count: n + 1)
+            }
+            for e in edges {
+              elist[counter[e.first]] = e.second
+              counter[e.first] += 1
+            }
+          }
+          elistCount = edges.count
         }
+
+        return (start, elist)
       }
     }
   }
 }
-
-extension _Internal.csr {
-  
-  @usableFromInline
-  struct Header {
-    @inlinable
-    @inline(__always)
-    internal init(capacity: Int) {
-      self.capacity = capacity
-    }
-    @usableFromInline var capacity: Int
-  }
-
-  @_fixed_layout
-  @usableFromInline
-  final class Buffer<Element>: ManagedBuffer<Header, Element> {
-    
-    @inlinable
-    deinit {
-      self.withUnsafeMutablePointers { header, elements in
-        elements.deinitialize(count: header.pointee.capacity)
-        header.deinitialize(count: 1)
-      }
-    }
-    
-    @nonobjc
-    @inlinable
-    @inline(__always)
-    var pointer: UnsafeMutablePointer<Element> {
-      withUnsafeMutablePointerToElements({ $0 })
-    }
-    
-    @nonobjc
-    @inlinable
-    public subscript(index: Int) -> Element {
-      _read { yield pointer[index] }
-      _modify { yield &pointer[index] }
-    }
-    
-    @nonobjc
-    @inlinable
-    @inline(__always)
-    internal static func create(
-      withCapacity capacity: Int
-    ) -> Buffer<Element> {
-      let storage = Buffer<Element>.create(minimumCapacity: capacity) { _ in
-        Header(capacity: capacity)
-      }
-      return unsafeDowncast(storage, to: Buffer<Element>.self)
-    }
-  }
-}
-#endif
