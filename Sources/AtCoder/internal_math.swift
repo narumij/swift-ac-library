38acc2515055023caf68a3d67a99ec41210fc430
narumij <narumij@gmail.com>
2025-06-12 23:32:26 +0900

    some

--- Sources/AtCoder/internal_math.swift
+++ Sources/AtCoder/internal_math.swift
@@ -238,7 +238,7 @@ extension _Internal {
     while true {
       if a >= m {
         let a_m = a.quotientAndRemainder(dividingBy: m)
-        ans &+= n * (n &- 1) / 2 &* a_m.quotient
+        ans &+= n &* (n &- 1) / 2 &* a_m.quotient
         a = a_m.remainder
       }
       if b >= m {
