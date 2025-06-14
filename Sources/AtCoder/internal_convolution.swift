8bc8f4d7dd0da7596f74d665eab4894d4cbd4476
narumij <narumij@gmail.com>
2025-06-14 11:31:48 +0900

    some

--- Sources/AtCoder/internal_convolution.swift
+++ Sources/AtCoder/internal_convolution.swift
@@ -1,29 +1,4 @@
-
-//extension Array where Element: AdditiveArithmetic {
-//  @inlinable
-//  public mutating func resize(_ n: Int) {
-//    guard count != n else { return }
-//    if count > n {
-//      removeLast(count - n)
-//      return
-//    }
-//    reserveCapacity(Swift.max(count, n))
-//    append(contentsOf: repeatElement(.zero, count: n - count))
-//  }
-//}
-//
-//extension ArraySlice where Element: AdditiveArithmetic {
-//  @inlinable
-//  public mutating func resize(_ n: Int) {
-//    guard count != n else { return }
-//    if count > n {
-//      removeLast(count - n)
-//      return
-//    }
-//    reserveCapacity(Swift.max(count, n))
-//    append(contentsOf: repeatElement(.zero, count: n - count))
-//  }
-//}
+import Foundation
 
 extension static_modint {
 
