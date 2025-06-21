abcf349294d999c6fdf1f0733e59b69cebb95a4d
narumij <narumij@gmail.com>
2025-06-21 23:36:25 +0900

    some, test

--- Sources/AtCoder/internal_mod.swift
+++ Sources/AtCoder/internal_mod.swift
@@ -218,7 +218,7 @@ extension modint_raw { // Numeric Comformance
   }
   
   public var magnitude: UInt.Magnitude {
-    val.magnitude
+    _v.magnitude
   }
   
   public typealias Magnitude = UInt.Magnitude
